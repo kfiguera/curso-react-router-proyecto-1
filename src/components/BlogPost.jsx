fe542e395e9ecfcabadb14dbfd9c555d011b8bc4
Kevin Figuera <kevin.figuera@techtool.io>
2023-01-06 22:15:34 -0400

    useNavigate: historial de navegación

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { useParams} from "react-router";
+import { Link, useNavigate, useParams} from "react-router-dom";
 import blogData from "../fixtures/blockData";
 
 function BlogPost() {
+    const navigate = useNavigate();
     const {slug} = useParams();
     const blogPost = blogData.find(post => post.slug === slug);
+
+    const returnToBlog = () => {
+        navigate('/blog');
+    };
     return (
         <React.Fragment>
             <h2>{blogPost.title}</h2>
             <p>{blogPost.content}</p>
             <p>Author: {blogPost.author}</p>
+            <button onClick={returnToBlog}>Volver al blog</button>
         </React.Fragment>
     );
 }
